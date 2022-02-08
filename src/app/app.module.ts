c739956f4cd3e068323c51f7515a071a78bcb7d2
Sorin <Sorin.Bogde@Chubb.com>
2022-02-08 20:02:21 +0000

    `$ ng generate component contacts-list`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsListComponent
   ],
   imports: [
     BrowserModule
