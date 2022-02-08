f1c7ed02ceeac0fc81ea00e244f73b21aeedbcfa
Sorin <Sorin.Bogde@Chubb.com>
2022-02-08 18:31:46 +0000

    app.component new template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'angular11-contacts-list';
+  rows = [...Array(10).keys()];
+
+  ngOnInit(){
+    console.log(this.rows)
+  }
 }
